2602d20fe7da636d5a3e412ca509500ed4eb6cca
bailram <>
2021-05-11 14:43:35 +0700

    repair rest api respon

--- src/main/java/com/bailram/restfullapi/controller/MahasiswaController.java
+++ src/main/java/com/bailram/restfullapi/controller/MahasiswaController.java
@@ -3,7 +3,9 @@ package com.bailram.restfullapi.controller;
 import com.bailram.restfullapi.model.Mahasiswa;
 import com.bailram.restfullapi.service.MahasiswaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,27 +17,37 @@ public class MahasiswaController {
     MahasiswaService mahasiswaService;
 
     @GetMapping("/")
-    public List<Mahasiswa> findAll() {
-        return mahasiswaService.findAll();
+    public ResponseEntity<List<Mahasiswa>> findAll() {
+        List<Mahasiswa> result = mahasiswaService.findAll();
+        if (result != null && !result.isEmpty()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/{nim}")
-    public Mahasiswa findByNim(@PathVariable(value = "nim") int nim) {
-        return mahasiswaService.findByNim(nim);
+    public ResponseEntity<Mahasiswa> findByNim(@PathVariable(value = "nim") int nim) {
+        Mahasiswa result = mahasiswaService.findByNim(nim);
+        if (result != null || result.getNim() != 0) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping(value = "add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String addMahasiswa(@RequestBody Mahasiswa mahasiswa) {
+    public ResponseEntity<String> addMahasiswa(@RequestBody Mahasiswa mahasiswa) {
         return mahasiswaService.addMahasiswa(mahasiswa);
     }
 
     @PutMapping("/update/{nim}")
-    public String updateMahasiswa(@PathVariable(value = "nim") int nim, @RequestBody Mahasiswa mahasiswa) {
+    public ResponseEntity<String> updateMahasiswa(@PathVariable(value = "nim") int nim, @RequestBody Mahasiswa mahasiswa) {
         return mahasiswaService.updateMahasiswa(nim, mahasiswa);
     }
 
     @DeleteMapping("/delete/{nim}")
-    public String deleteMahasiswa(@PathVariable(value = "nim") int nim) {
+    public ResponseEntity<String> deleteMahasiswa(@PathVariable(value = "nim") int nim) {
         return mahasiswaService.deleteMahasiswa(nim);
     }
 }
--- src/main/java/com/bailram/restfullapi/service/MahasiswaService.java
+++ src/main/java/com/bailram/restfullapi/service/MahasiswaService.java
@@ -3,6 +3,8 @@ package com.bailram.restfullapi.service;
 import com.bailram.restfullapi.model.Mahasiswa;
 import com.bailram.restfullapi.repository.MahasiswaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,36 +22,48 @@ public class MahasiswaService {
         return mahasiswaRepository.findByNim(nim);
     }
 
-    public String addMahasiswa(Mahasiswa mahasiswa) {
-        mahasiswaRepository.save(mahasiswa);
-        return "Successfully Add Mahasiswa";
+    public ResponseEntity<String> addMahasiswa(Mahasiswa mahasiswa) {
+        try {
+            mahasiswaRepository.save(mahasiswa);
+            return new ResponseEntity<>("Successfully Add Mahasiswa", HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>("Error occured: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public String updateMahasiswa(int nim, Mahasiswa mahasiswa) {
-        Mahasiswa existMahasiswa = mahasiswaRepository.findByNim(nim);
-        if (existMahasiswa == null) {
-            return "Mahasiswa with NIM "+nim+" Not Found!";
+    public ResponseEntity<String> updateMahasiswa(int nim, Mahasiswa mahasiswa) {
+        try {
+            Mahasiswa existMahasiswa = mahasiswaRepository.findByNim(nim);
+            if (existMahasiswa == null) {
+                return new ResponseEntity<>("Mahasiswa with NIM "+nim+" Not Found!", HttpStatus.NOT_FOUND);
+            }
+            if (!mahasiswa.getNama().isEmpty()) {
+                existMahasiswa.setNama(mahasiswa.getNama());
+            }
+            if (!mahasiswa.getJurusan().isEmpty()) {
+                existMahasiswa.setJurusan(mahasiswa.getJurusan());
+            }
+            if (mahasiswa.getIpk() != 0.0f) {
+                existMahasiswa.setIpk(mahasiswa.getIpk());
+            }
+            mahasiswaRepository.save(existMahasiswa);
+            return new ResponseEntity<>("Successfully update Mahasiswa with NIM "+nim, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error occured: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        if (!mahasiswa.getNama().isEmpty()) {
-            existMahasiswa.setNama(mahasiswa.getNama());
-        }
-        if (!mahasiswa.getJurusan().isEmpty()) {
-            existMahasiswa.setJurusan(mahasiswa.getJurusan());
-        }
-        if (mahasiswa.getIpk() != 0.0f) {
-            existMahasiswa.setIpk(mahasiswa.getIpk());
-        }
-        mahasiswaRepository.save(existMahasiswa);
-        return "Successfully update Mahasiswa with NIM "+nim;
     }
 
-    public String deleteMahasiswa(int nim) {
-        Mahasiswa existMahasiswa = mahasiswaRepository.findByNim(nim);
-        if (existMahasiswa == null) {
-            return "Mahasiswa with NIM "+nim+" Not Found!";
-        }
-        mahasiswaRepository.deleteById(nim);
+    public ResponseEntity<String> deleteMahasiswa(int nim) {
+        try {
+            Mahasiswa existMahasiswa = mahasiswaRepository.findByNim(nim);
+            if (existMahasiswa == null) {
+                return new ResponseEntity<>("Mahasiswa with NIM "+nim+" Not Found!", HttpStatus.NOT_FOUND);
+            }
+            mahasiswaRepository.deleteById(nim);
 
-        return "Successfully delete Mahasiswa with NIM "+nim;
+            return new ResponseEntity<>("Successfully delete Mahasiswa with NIM "+nim, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>("Error occured: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
